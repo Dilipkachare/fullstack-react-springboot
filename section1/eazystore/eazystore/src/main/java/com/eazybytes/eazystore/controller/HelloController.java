89307ece33d1bd6e6c1f560bd8723b2cee372a3d
Dilipkachare <dilipkachare888@gmail.com>
2026-07-14 20:38:54 +0530

    initial setup

--- section1/eazystore/eazystore/src/main/java/com/eazybytes/eazystore/controller/HelloController.java
+++ section1/eazystore/eazystore/src/main/java/com/eazybytes/eazystore/controller/HelloController.java
@@ -1,4 +1,14 @@
 package com.eazybytes.eazystore.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+
 }
