48aef6f02c5bf5c8bfce5555545c0be52b1b53f3
Dilipkachare <dilipkachare888@gmail.com>
2026-07-20 19:21:10 +0530

    .

--- section4/eazystore-ui/src/components/ClassHeader.jsx
+++ section4/eazystore-ui/src/components/ClassHeader.jsx
@@ -11,30 +11,30 @@ class ClassHeader extends Component {
             <FontAwesomeIcon icon={faTags} className="fa-icon" />
             <span className="brand-title">Eazy Stickers</span>
           </a>
-          <nav className="nav">
+          <nav className="eazhynav">
             <ul>
               <li>
-                <a href="/" className="nav-link">
+                <a href="/" className="navLink">
                   Home
                 </a>
               </li>
               <li>
-                <a href="/about" className="nav-link">
+                <a href="/about" className="navLink">
                   About
                 </a>
               </li>
               <li>
-                <a href="/contact" className="nav-link">
+                <a href="/contact" className="navLink">
                   Contact
                 </a>
               </li>
               <li>
-                <a href="/login" className="nav-link">
+                <a href="/login" className="navLink">
                   Login
                 </a>
               </li>
               <li>
-                <a href="/cart" className="nav-link">
+                <a href="/cart" className="navLink">
                   <FontAwesomeIcon icon={faShoppingBasket} />
                 </a>
               </li>
--- section4/eazystore-ui/src/components/Home.jsx
+++ section4/eazystore-ui/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import products from "../data/products";
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
+import BootstrapButton from "./BootstrapButton";
 
 
 
@@ -21,6 +22,34 @@ export default function Home() {
    {/* <h1 style={h1styles}>Welcome to Eazy Stickers!</h1> */}
   
     <div className="home-container">
+      <div className="container" col-6>
+      <BootstrapButton text="Submit" type="primary" />
+      <BootstrapButton text="Save" type="secondary" />
+      <BootstrapButton text="Delete" type="danger" />
+      <BootstrapButton text="Submit" type="primary" />
+      </div>
+      <div>
+        <div className="d-grid gap-2 col-8 mx-auto"></div>
+        <div className="alert alert-primary text-center" role="alert">
+  A simple primary alert—check it out!
+</div>
+<div className="alert alert-secondary text-center text-center" role="alert">
+  A simple secondary alert—check it out!
+</div>
+<div className="alert alert-success text-center" role="alert">
+  A simple success alert—check it out!
+</div>
+<div className="alert alert-danger text-center" role="alert">
+  A simple danger alert—check it out!
+
+  <button 
+  type="button" 
+  className="btn-close"
+   data-bs-dismiss="alert"
+    aria-label="Close"
+    ></button>
+</div>
+      </div>
       <PageHeading title="Explore Eazy Stickers!">
         Add a touch of creativity to your space with our wide range of fun and
         unique stickers. Perfect for any occasion!
