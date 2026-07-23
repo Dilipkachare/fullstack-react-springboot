117e20eea79b707eebcd9e7ba08d5da49031dc7b
Dilipkachare <dilipkachare888@gmail.com>
2026-07-23 20:04:12 +0530

    Building Spring Application

---
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.eazybytes.eazystore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+    @RequestMapping("/products")
+    public String getProduct(){
+        return "Product details";
+    }
+}
