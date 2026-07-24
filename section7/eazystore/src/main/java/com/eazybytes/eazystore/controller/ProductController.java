42b10030b5f3f3ae880c78b89eae9b8d374106c8
Dilipkachare <dilipkachare888@gmail.com>
2026-07-24 16:28:07 +0530

    .

--- section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -1,14 +1,24 @@
 package com.eazybytes.eazystore.controller;
 
+import com.eazybytes.eazystore.entity.Product;
+import com.eazybytes.eazystore.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductRepository productRepository;
+
+
     @RequestMapping("/products")
-    public String getProduct(){
-        return "Product details";
+    public List<Product> getProduct(){
+        List<Product> productsList = productRepository.findAll();
+        return productsList;
     }
 }
