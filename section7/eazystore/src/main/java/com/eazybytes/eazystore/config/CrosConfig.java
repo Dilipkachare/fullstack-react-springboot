d12ebb57f3126e66e18998b37e4df200a78376f2
Dilipkachare <dilipkachare888@gmail.com>
2026-07-27 16:00:06 +0530

    .

---
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/config/CrosConfig.java
@@ -0,0 +1,24 @@
+package com.eazybytes.eazystore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class CrosConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration config=new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**" , config);
+        return  new CorsFilter(source);
+    }
+}
