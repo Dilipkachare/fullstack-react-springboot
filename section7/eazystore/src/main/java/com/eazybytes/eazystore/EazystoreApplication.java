54b7b73fd4df0dde0dbc9b1453c4635920e4af72
Dilipkachare <dilipkachare888@gmail.com>
2026-07-28 00:30:34 +0530

    .

--- section7/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
+++ section7/eazystore/src/main/java/com/eazybytes/eazystore/EazystoreApplication.java
@@ -2,8 +2,12 @@ package com.eazybytes.eazystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories
+//@EntityScan
 public class EazystoreApplication {
 
 	public static void main(String[] args) {
